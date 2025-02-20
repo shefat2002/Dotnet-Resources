90131bb002b8fa7b0a9867a0eaf4cfea2b3b7690
shefat2002 <shefat2002@gmail.com>
2025-02-20 18:04:47 +0600

    add Enum

---
+++ CSharp/OOP/Enum/Program.cs
@@ -0,0 +1,36 @@
+﻿//enum is a value type data type. It is used to declare a list of named integer constants.
+
+internal class Program
+{
+    // Enum declaration
+    enum Language
+    {
+        PL,
+        DE
+    }
+    static void Main(string[] args)
+    {
+        
+        Language language = Language.PL;
+
+        var ans = language switch{
+            Language.PL => "Polish",
+            Language.DE => "German",
+        };
+
+        Console.WriteLine(ans);
+        // switch(language)
+        // {
+            
+        //     case Language.PL:
+        //         Console.WriteLine("Polish");
+        //         break;
+        //     case Language.DE:
+        //         Console.WriteLine("German");    
+        //         break;
+        //     default:
+        //         Console.WriteLine("Engilish");
+        //         break;
+        // }
+    }
+}
---
+++ CSharp/OOP/Enum/Readme.md
@@ -0,0 +1,3 @@
+<h1>Enum</h1>
+An enum is a special "class" that represents a group of constants (unchangeable/read-only variables).<br>
+Enum is short for "enumerations", which means "specifically listed".
