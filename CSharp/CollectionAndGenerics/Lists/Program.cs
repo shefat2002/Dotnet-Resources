d3fdefc5938bb7e53ef596410812de00aef2ffb2
shefat2002 <shefat2002@gmail.com>
2025-02-22 17:35:17 +0600

    Add List Code

---
+++ CSharp/CollectionAndGenerics/Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿List<string> list= new(){
+    "Rahim", "Karim","Alex","Jack","Joe", "Jabbar", "Abdulla", "Shefat","Bob", "Abc"
+};
+
+foreach (var item in list)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+//Uppercase All
+foreach (var item in list)
+{
+    Console.Write(item.ToUpper() + " ");
+}
+
+Console.WriteLine();
+
+//Add new items to list
+list.Add("Cde");
+list.Add("Xyz");
+
+//Total Size
+Console.WriteLine($"Total Names: {list.Count}");
+
+//Last Element
+Console.WriteLine(list[list.Count-1]);
+
+//Searching
+var toFind = "Abc";
+if(list.IndexOf(toFind) !=-1){
+    Console.WriteLine($"{toFind} found in list");
+}
+
+//Sorting
+list.Sort();
+foreach(var item in list){
+    Console.Write(item + " ");
+}
+
+
+Console.WriteLine();
