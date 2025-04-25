20fb2ce2f6a2ee078eacc860011b39cc7aa83402
shefat2002 <shefat2002@gmail.com>
2025-04-25 19:23:21 +0600

    add

---
+++ CSharp/CollectionAndGenerics/Dictonary/Program.cs
@@ -0,0 +1,13 @@
+﻿Dictionary<int,string> dict = new Dictionary<int, string>();
+dict.Add(101,"ABC");
+dict.Add(102,"ABC");
+dict.Add(103,"ABC");
+dict.Add(104,"ABC");
+
+
+// Dictonary<int,string> dic = new();
+
+foreach(var d in dict)
+{
+    Console.WriteLine($"ID: {d.Key}, Name: {d.Value}");
+}
